e391a9dd98a29b507d53f29d0ed9ff2c336217cd
Anastasiia Krug <78438196+anastasiia-krug@users.noreply.github.com>
2025-02-14 11:44:18 +0300

    Styles, Contact mvc, contacts form.

---
+++ WebSite/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebSite/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSite.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage ="Вам необходимо обязательно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам необходимо обязательно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам необходимо обязательно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам необходимо обязательно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Вам необходимо обязательно ввести сообщение")]
+        public string Message { get; set; }
+    }
+}
